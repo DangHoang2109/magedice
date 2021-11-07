3bde8200f65d6657c60b6fb07d25901b448ad6ff
danghoang2109 <danghoang2109@gmail.com>
2021-11-07 11:14:37 +0700

    - add mage dice resource

---
+++ MageDice/Assets/MageDice/Scripts/Bullet/BaseBullet.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class BaseBullet : MonoBehaviour
+{
+    [SerializeField] private Transform Enemy;
+
+    [SerializeField] private float _ConfigDistance = 5;
+    [SerializeField] private float _speed;
+    private float FlyTime => _ConfigDistance / _speed;
+    private Vector3 velocity = Vector3.zero;
+
+    bool isFlying;
+
+    [ContextMenu("Shoot")]
+    public void Shooted()
+    {
+        isFlying = true;
+        //float distance = GameUtils.DistanceBetween(this.transform, Enemy);
+
+        //DOTween.To(() => this.transform.transform.position, x => this.transform.transform.position = x, desPosition, distance / this._speed)
+        //.SetEase(Ease.Linear)
+        //.OnUpdate(() =>
+        //{
+        //    Debug.Log($"Aim to {Enemy.transform.position}");
+        //    desPosition = Enemy.transform.position;
+        //    Debug.Log($"Bullet {this.transform.transform.position} fly to {desPosition}");
+
+        //})
+        //.OnComplete(Hitted);
+
+        //this.transform.DOMove(Enemy.transform.position, distance / this._speed)
+        //    .SetEase(Ease.Linear)
+        //    .OnComplete(this.Hitted);
+    }
+
+
+    private void Update()
+    {
+        if(isFlying && this.Enemy != null)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, Enemy.transform.position, ref velocity, FlyTime);
+            Debug.Log($"Bullet {this.transform.position} fly to {Enemy.transform.position}");
+            if (GameUtils.IsNear(transform.position, Enemy.transform.position, 20)) //pixel
+            {
+                Hitted();
+            }
+
+            //transform.position = Vector3.MoveTowards(transform.position, Enemy.transform.position,
+            //    step / moveTime * Time.deltaTime);
+
+            //this.transform.position = Vector3.Lerp(this.transform.position, Enemy.transform.position, t);
+        }
+    }
+
+    public void Hitted()
+    {
+        Debug.Log("Bullet hit enemy");
+        isFlying = false;
+    }
+}
---
+++ MageDice/Assets/MageDice/Scripts/Monster/BaseMonsterBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class BaseMonsterBehavior : MonoBehaviour
+{
+    public float Distance;
+
+    [SerializeField] private Transform Tower;
+    [SerializeField] private float _speed;
+
+    private float TimeMove => Distance / _speed;
+
+    [ContextMenu("Run")]
+    public void Run()
+    {
+        this.transform.DOMoveY(Tower.position.y, TimeMove)
+            .SetEase(Ease.Linear)
+            .OnComplete(AttackTower);
+    }
+
+    public void AttackTower()
+    {
+        Debug.Log("Reach Tower");
+    }
+
+    public void Hitted()
+    {
+
+    }
+}
